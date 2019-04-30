9ebea3e976e0caa61e0ba77ff2bc8a46188398fa
eric-do <ericdo.617@gmail.com>
2019-04-30 13:05:50 -0700

    Solve pluckFirstLineFromFile

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,12 +6,16 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, (err, data) => {
+    if (err) { return callback(err); }
+    var dataArr = data.toString().split(`\n`);
+    callback(null, dataArr[0]);
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, callback) {
   // TODO
 };
 
